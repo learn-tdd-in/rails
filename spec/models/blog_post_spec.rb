ebca7c3cebe62a3d225ef8aee012cfc2f294400b
Josh Justice <jjustice@bignerdranch.com>
2016-06-14 12:30:58 -0400

    Specify blog post accessors
    Once again, rather than fixing the acceptance error directly, we drop down to the model test level to reproduce the error. We specify that we want a `title` field--as well as a `body` field, since we know we'll need that and it feels safe to go ahead and add it. The behavior we want is that we want those fields to be `nil` for a new model instance.
    
    Inner red: undefined method `title' for #<BlogPost id: nil>
    
    With this specification, we've reproduced the error from the acceptance test.

--- spec/models/blog_post_spec.rb
+++ spec/models/blog_post_spec.rb
@@ -6,4 +6,11 @@ describe BlogPost do
     expect{ blog_post = BlogPost.new }.not_to raise_error
   end
 
+  it "defaults fields to nil" do
+    blog_post = BlogPost.new
+
+    expect(blog_post.title).to be_nil
+    expect(blog_post.body).to be_nil
+  end
+
 end
