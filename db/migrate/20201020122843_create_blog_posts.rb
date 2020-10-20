573fdf60e768fdf3d82aada73b9bec924091896e
Josh Justice <me@codingitwrong.com>
2020-10-20 08:30:45 -0400

    Add blog post model

---
+++ db/migrate/20201020122843_create_blog_posts.rb
@@ -0,0 +1,10 @@
+class CreateBlogPosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :blog_posts do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
